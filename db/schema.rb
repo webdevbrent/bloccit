cd706b6dd6a1009c7cd86a265e44716f5884d58a
webdevbrent <webdeveloperbrent@gmail.com>
2018-03-25 11:08:05 -0400

    db

--- db/schema.rb
+++ db/schema.rb
@@ -45,14 +45,6 @@ ActiveRecord::Schema.define(version: 20180325031001) do
     t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
-  # create_table "questions", force: :cascade do |t|
-  #   t.string "title"
-  #   t.text "body"
-  #   t.boolean "resolved"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-
   create_table "topics", force: :cascade do |t|
     t.string "name"
     t.boolean "public", default: true
