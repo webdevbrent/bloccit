3ab3cc1af1a501b0d06acd7f8b4a3ff72eb301e7
webdevbrent <webdeveloperbrent@gmail.com>
2018-04-09 11:23:25 -0400

    Created Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
   resources :users, only: %i[new create]
+  resources :sessions, only: %i[new create destroy]
   resources :advertisements
   resources :questions
 
