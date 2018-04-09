187aebf6ddaff1ce9d604459dd4e655c2ba4ba3c
webdevbrent <webdeveloperbrent@gmail.com>
2018-04-09 00:23:41 -0400

    Added Users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :questions
 
   get 'about' => 'welcome#about'
-
+  post 'users/confirm' => 'users#confirm'
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
